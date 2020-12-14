19dc9784c94d00de08418e2b3c1ebc3e31325b43
Chris Allbritton <cla.inbox@gmail.com>
2020-12-14 12:06:03 -0500

    Finish test to test all required attributes

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -25,5 +25,12 @@ feature "User can get detailed member info for a given nation" do
     expect(current_path).to eq("/search")
 
     expect(page).to have_content("Total Members: 20")
+
+    within(first('.member')) do
+      expect(page).to have_content("Chan (Fire Nation admiral)")
+      expect(page).to have_content("Affiliation: Fire Nation Navy")
+      expect(page).to have_content("Allies: Ozai")
+      expect(page).to have_content("Enemies: Earth Kingdom")
+    end
   end
 end
