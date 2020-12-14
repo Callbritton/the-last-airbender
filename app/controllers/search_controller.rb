8dbfa09861f0998d99dd09eb6a37fb34d0d381d3
Chris Allbritton <cla.inbox@gmail.com>
2020-12-14 12:41:45 -0500

    Refactor controller into service facade and poros

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,6 @@
 class SearchController < ApplicationController
   def index
     nation = params[:nation].split('_').join('+')
-    conn = Faraday.new(url: 'https://last-airbender-api.herokuapp.com')
-    response = conn.get("/api/v1/characters?affiliation=#{nation}")
-    parsed_data = JSON.parse(response.body, symbolize_names: true)
-
-    def fetch_member_data(parsed_data)
-      parsed_data.map do |data|
-        Member.new(data)
-      end
-    end
-
-    @members = fetch_member_data(parsed_data)
+    @members = SearchFacade.fetch_member_data(nation)
   end
 end
